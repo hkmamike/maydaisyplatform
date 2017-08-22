82d5c4afe3ecdf5f5e043351d1df549ed700f689
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-08-22 12:10:36 +0800

    how it works section done

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Grid, Row, Col, Glyphicon } from 'react-bootstrap';
 
 export default class Homepage extends Component {
  render() {
@@ -9,12 +9,25 @@ export default class Homepage extends Component {
           <div className="home-image"></div>
         </div>
 
-        <div className="home-how-it-works">
-          <div className="row">
-            <div className="large-4 columns">...</div>
-            <div className="large-4 columns">...</div>
-            <div className="large-4 columns">...</div>
-          </div>
+        <div className="how-it-works">
+          <h2> How <span className="home-company-name">One Bloom</span> Works </h2>
+          <h3 className="home-123"> AS SIMPLE AS 1,2,3 </h3>
+          <Grid>
+            <Row className="show-grid">
+              <Col sm={4}><Glyphicon glyph="list-alt" className="icons"/>
+                <h3 className="icon-title">Subscribe</h3>
+                <div className="icon-description">Send weekly mystery flowers to your loved one. Let us know who the lucky one is.</div>
+              </Col>
+              <Col sm={4}><Glyphicon glyph="question-sign" className="icons"/>
+                <h3 className="icon-title">Receive Update</h3>
+                <div className="icon-description">2 days before delivery, we email you the details about the upcoming flowers, and you can update your card on our website.</div>
+              </Col>
+              <Col sm={4}><Glyphicon glyph="heart" className="icons"/>
+                <h3 className="icon-title">Surprise</h3>
+                <div className="icon-description">Your loved one receive weekly mystery flower, prepared with heart by our designers and fresh cut from the farm!</div>
+              </Col>
+            </Row>
+          </Grid>
         </div>
 
       </div>
