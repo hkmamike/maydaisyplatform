bdb46e6163fa84c03f4d6c357f2a3ab981b26f82
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-09-02 15:06:32 +0800

    working on submitting subscriptions to firebase

--- src/components/helpers/payment.js
+++ src/components/helpers/payment.js
@@ -7,74 +7,112 @@ import { base } from '../config/constants';
 export default class ChargeMoney extends React.Component {
   onToken = (token) => {
     var uid = firebase.auth().currentUser.uid;
-    console.log('selectRegion : ', this.props.selectRegion);
+    var selectRegion = this.props.selectRegion;
+    var planID = this.props.planID;
+
+    var selectPlanType = this.props.selectPlanType;
+    var selectPlanSize = this.props.selectPlanSize;
+    var grandTotalPerWeek = this.props.grandTotal;
+
+    var senderName = this.props.sender;
+    var senderNum = this.props.senderNum;
+    var senderEmail = token.email;
+
+    var recipient = this.props.recipient;
+    var recipientNum = this.props.recipientNum;
+    var company = this.props.company;
+    var address = this.props.address;
+    var cardMessage = this.props.cardMessage;
+
+    var stripeTokID = token.id;
+    var stripeCusID;
+    var stripeSubID;
+
     console.log('sending token to webtask, token is : ', token);
     fetch('https://wt-47cf129daee3aa0bf6d4064463e232ef-0.run.webtask.io/webtask-stripe-order'
     +'?paymentSource=' + token.id
     +'&paymentEmail=' + token.email, {
       method: 'POST',
     }).then(response => {
-      response.json().then(data => {
-        console.log ('customer has been created: ', data);
-        console.log ('customer id is:', data.id);
-        console.log ('plandID is :', this.props.planID);
-        fetch('https://wt-47cf129daee3aa0bf6d4064463e232ef-0.run.webtask.io/webtask-stripe-payment' 
-        + '?customerID=' + data.id 
-        + '&planID=' + this.props.planID, {
-            method: 'POST',
-          })
-          .then(response => {
+        response.json().then(data => {
+            console.log ('customer has been created: ', data);
+            console.log ('customer id is:', data.id);
+            stripeCusID = data.id;
+            console.log ('plandID is :', this.props.planID);
+            fetch('https://wt-47cf129daee3aa0bf6d4064463e232ef-0.run.webtask.io/webtask-stripe-payment' 
+            + '?customerID=' + data.id 
+            + '&planID=' + this.props.planID, {
+                method: 'POST',
+            })
+            .then(response => {
                 response.json().then(data => {
                     console.log ('subscription response: ', data);
+                    stripeSubID = data.id;
                     console.log ('2check to see if token is visible: ', token);
+                    base.update(`allSubscriptions/hongKong/${selectRegion}/${planID}/${uid}`, {
+                        data: {
+                              selectPlanType: selectPlanType,
+                              selectPlanSize: selectPlanSize,
+                              grandTotalPerWeek: grandTotalPerWeek,
+                              senderName: senderName,
+                              senderNum: senderNum,
+                              senderEmail: senderEmail,
+                              recipient: recipient,
+                              recipientNum: recipientNum,
+                              company: company,
+                              address: address,
+                              cardMessage: cardMessage,
+                              stripeTokID: stripeTokID,
+                              stripeCusID: stripeCusID,
+                              stripeSubID: stripeSubID
+                        }
+                    });
                 });
-          });
-      });
-    });
-  }
+            });
+        });
+    })
+}
  
 
 //   submitSubscription (
-//     //     token,
-//     //     uid,
-//     //     stripeCusID,
-//     //     selectRegion,
-//     //     planID, 
-//     //     flowers, 
-//     //     size, 
-//     //     sender,
-//     //     senderNum,
-//     //     senderEmail,
-//     //     recipient, 
-//     //     recipientNum, 
-//     //     comapny, 
-//     //     address,
-//     //     cardMessage,
-//     //     pricePerWeek) {
-//     // base.push(`allSubscriptions/hongKong/${selectRegion}/${planID}/${stripeCusID}`, {
-//     //   data: {
-//     //         paymentToken: token.id,
-//     //         flowers: flowers,
-//     //         size: size,
-//     //         sender: sender,
-//     //         senderNum: senderNum,
-//     //         senderEmail, senderEmail,
-//     //         senderID: uid,
-//     //         recipient: recipient,
-//     //         recipientNum: recipientNum,
-//     //         addressLocation: company,
-//     //         address: address,
-//     //         cardMessage: cardMessage,
-//     //         price: pricePerWeek
-//     //     }
-//     // });
-//     //available immediately, you don't have to wait for the callback to be called 
-//     // var generatedKey = immediatelyAvailableReference.key;
+//         token,
+//         uid,
+//         stripeCusID,
+//         selectRegion,
+//         planID, 
+//         flowers, 
+//         size, 
+//         sender,
+//         senderNum,
+//         senderEmail,
+//         recipient, 
+//         recipientNum, 
+//         comapny, 
+//         address,
+//         cardMessage,
+//         pricePerWeek) {
+//     base.update(`allSubscriptions/hongKong/${selectRegion}/${planID}/${stripeCusID}`, {
+//       data: {
+//             paymentToken: token.id,
+//             flowers: flowers,
+//             size: size,
+//             sender: sender,
+//             senderNum: senderNum,
+//             senderEmail, senderEmail,
+//             senderID: uid,
+//             recipient: recipient,
+//             recipientNum: recipientNum,
+//             addressLocation: company,
+//             address: address,
+//             cardMessage: cardMessage,
+//             price: pricePerWeek
+//         }
+//     });
+//     var generatedKey = immediatelyAvailableReference.key;
 //     this.setState({loading: false, formSubmitted: true});
 //   }
 
   // ...
- 
   render() {
     return (
       // ...
