d2163be77873380589a694f45f887355c6f68e48
Brian Leung (Mike) <mikebrianleung@gmail.com>
2017-10-02 18:03:59 +0800

    trying to change meta data

--- src/components/gallery/elegant.js
+++ src/components/gallery/elegant.js
@@ -10,13 +10,13 @@ let strings = new LocalizedStrings({
       classic: 'Classic',
       elegant: 'Elegant',
       bloom: 'Bloom',
-      noteOn: 'Note on',
+      noteOn: 'Note on ',
       planName: 'Elegant',
       li1: 'For HKD93 per week, this plan is a weekly unrestricted flower design and delivery service prepared by our florists.',
       li2: "While each week's design is different, Elegant most often consists of 2-4 major blooms, sometimes supported by minor flowers.",
       li3: 'Office locations receive wrapped arrangement, while home and cemetery locations receive vase arrangement.',
       li4: 'For vase arrangement, please provide your vase in an area where our florist can access. For home locations, please put your vase in front of your door if no one is at home.',
-      li5: "For the Elegant plan, the ideal vase height is 10-15cm and the ideal vase opening's diameter is 2-3cm.",
+      li5: "For the Elegant plan, the ideal vase height is 10-15cm and the ideal vase opening's diameter is 3-5cm.",
       li6: 'To make MayDaisy affordable to flower lovers, we begin to invite customers to subscribe when 150 customers have shown interest in an area. Check out if we are servicing your area now!'
     },
     ch: {
@@ -31,7 +31,7 @@ let strings = new LocalizedStrings({
       li2: '我們的花匠會用時令花材設計您的花卉。「優雅」計劃的設計雖然每週不同，但一般會有2-4朵主花及以適合的配花作襯托。',
       li3: '辦公室地點會以花束配送，而住家和墓園地點會以到場插花的形式配送。',
       li4: '如果您訂購了到場插花的服務，請將花瓶放在一個花匠可以到達的地方。以住家地點為例，如果配送當天家中沒有人，請將花瓶放在門前一個安全的位置。',
-      li5: '建議花瓶尺寸:「優雅」計劃的設計配以一個 10-15cm 高，開口直徑 2-3cm 大的花瓶效果會較好。',
+      li5: '建議花瓶尺寸:「優雅」計劃的設計配以一個 10-15cm 高，開口直徑 3-5cm 大的花瓶效果會較好。',
       li6: '為了令五月菊價格更大眾化，每個地區的服務會在收集到150個報名之後開啟，屆時已報名的客人會收到電郵邀請。快來看看您的地區服務是否已開啟!'
     }
   });
